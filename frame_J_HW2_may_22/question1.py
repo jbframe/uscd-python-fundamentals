bd0a5b6358ffed72e1fb43ca5f65e0f6873a9e37
jbframe <framejb@gmail.com>
2021-05-22 22:35:58 -0600

    Question 1 complete

--- frame_J_HW2_may_22/question1.py
+++ frame_J_HW2_may_22/question1.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # Assignment 2
 # Question 1
-# Give a list saved in a variable: a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100] write one line of Python that takes this list and makes a new list that has only the even elements of this list in it.
+# Given a list saved in a variable: a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100] write one line of Python that takes this list and makes a new list that has only the even elements of this list in it.
 
 a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+evenElementsList = [i for i in a if i%2==0]
+
+# test
+print(evenElementsList) # [4, 16, 36, 64, 100]
