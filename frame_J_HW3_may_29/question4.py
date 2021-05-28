8d6473769c591fb89bd3b0b97dd7907849e70489
jbframe <framejb@gmail.com>
2021-05-27 22:01:47 -0600

    Question 4 complete.

--- frame_J_HW3_may_29/question4.py
+++ frame_J_HW3_may_29/question4.py
@@ -4,10 +4,13 @@
 # Write a Python program that counts how many lines and how many words there are in a file. Use the file attached here  download > About_Python-1.txt.
 def countLinesAndWords(fileName):
   fHandle = open('About_Python-1.txt')
-
-  count = 0
+  lineCount = 0
+  wordCount = 0
   for line in fHandle:
-      count += 1
-  print(count)
+    lineCount += 1
+    wordCount += len(line.rstrip().split(' '))
+  print("The file " + str(fileName) + " has " + str(lineCount) + " lines and " + str(wordCount) + " words.")
+
+# Tests
 fileName = 'About_Python-1.txt'
-countLinesAndWords(fileName)
+countLinesAndWords(fileName) # Expected result "The file About_Python-1.txt has 7 lines and 102 words."
