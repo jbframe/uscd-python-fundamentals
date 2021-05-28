cd5837fdf07ff1b3dbd0b919a2acdb4867e3408d
jbframe <framejb@gmail.com>
2021-05-27 20:23:05 -0600

    Counting the lines in files.

--- frame_J_HW3_may_29/question4.py
+++ frame_J_HW3_may_29/question4.py
@@ -2,3 +2,12 @@
 # Assignment 3
 # Question 4
 # Write a Python program that counts how many lines and how many words there are in a file. Use the file attached here  download > About_Python-1.txt.
+def countLinesAndWords(fileName):
+  fHandle = open('About_Python-1.txt')
+
+  count = 0
+  for line in fHandle:
+      count += 1
+  print(count)
+fileName = 'About_Python-1.txt'
+countLinesAndWords(fileName)
