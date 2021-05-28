c5f2841dc9432fbc4982362d3aaead7bb2ca019b
jbframe <framejb@gmail.com>
2021-05-28 12:25:14 -0600

    Question 2 complete.

--- frame_J_HW3_may_29/question2.py
+++ frame_J_HW3_may_29/question2.py
@@ -2,19 +2,28 @@
 # Assignment 3
 # Question 2
 # Write a program that inherits from the class below and adds a modulus division function to the child class (test your code by creating and object and printing out the results for each function):
-# class Calc():
+class Calc():
+  def add(self, a, b):
+    return  a + b
 
-#     def add(self, a, b):
-#         return  a + b
+  def subtract(self, a, b):
+    return a - b
 
-#     def subtract(self, a, b):
-#         return a - b
+  def multiply(self,a,b):
+    return a * b
 
-#     def multiply(self,a,b):
-#         return a * b
+  def divide(self, a, b):
+    if b==0:
+      raise ValueError('Cannot divide by zero!')
+    return a / b
 
-#     def divide(self, a, b):
-#         if b==0:
-#             raise ValueError('Cannot divide by zero!')
-#         return a / b
+class Modulus(Calc):
+  def modulus(self, a, b):
+    return  a % b
 
+instanceOfModulusClass = Modulus()
+print(instanceOfModulusClass.add(10,2)) # Expect 12
+print(instanceOfModulusClass.subtract(10,2)) # Expect 8
+print(instanceOfModulusClass.multiply(10,2)) # Expect 20
+print(instanceOfModulusClass.divide(10,2)) # Expect 5.0
+print(instanceOfModulusClass.modulus(10,2)) # Expect 0
