11ce022101c0f663a1e34c08c2d1a8db0195a77d
jbframe <framejb@gmail.com>
2021-05-27 23:24:28 -0600

    Question 5 complete.

--- frame_J_HW3_may_29/question4.py
+++ frame_J_HW3_may_29/question4.py
@@ -3,7 +3,7 @@
 # Question 4
 # Write a Python program that counts how many lines and how many words there are in a file. Use the file attached here  download > About_Python-1.txt.
 def countLinesAndWords(fileName):
-  fHandle = open('About_Python-1.txt')
+  fHandle = open(fileName)
   lineCount = 0
   wordCount = 0
   for line in fHandle:
--- frame_J_HW3_may_29/question5.py
+++ frame_J_HW3_may_29/question5.py
@@ -2,3 +2,22 @@
 # Assignment 3
 # Question 4
 # Write a Python program that reads in the file from the problem above and writes the document completely backward to a new text file (the last letter of the entire document becomes the first and so on so the document can be read from bottom to top and from right to left)
+def writeDocumentBackwardToFile(fileName):
+  fHandle = open(fileName)
+  resultLineList = []
+  result = ""
+  newFHandle = open("DocumentBackward.txt", "w")
+  for line in fHandle:
+    #Assuming that we want to strip excess spaces and keep the same line breaks.
+    stripedLine = line.rstrip()
+    resultLineList.append(stripedLine[::-1])
+  resultLineList.reverse()
+  for i in range(len(resultLineList)):
+    result += resultLineList[i]
+    if i != len(resultLineList) - 1:
+      result += '\n'
+  newFHandle.write(result)
+
+# Tests
+fileName = 'About_Python-1.txt'
+writeDocumentBackwardToFile(fileName) # Expected result: A new file called DocumentBackward.txt with backwards txt is created
