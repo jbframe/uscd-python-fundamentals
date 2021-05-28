692ac3a3ea1064d6ff187e6c4cb07a684b18e0de
jbframe <framejb@gmail.com>
2021-05-28 00:25:18 -0600

    Question 1 complete.

--- frame_J_HW3_may_29/question1.py
+++ frame_J_HW3_may_29/question1.py
@@ -2,3 +2,15 @@
 # Assignment 3
 # Question 1
 # Write a Python class which has two methods get_String and print_String. The function get_String accepts and returns a string passed into a constructor. The function print_String prints the string in upper case. The class should have a constructor that returns the string entered by the user as an argument. Create an object to test execute the functions from the class.
+
+class Strings:
+  def __init__(self, userString):
+    self.userString = userString
+  def get_String(self):
+    return self.userString
+  def print_String(self):
+    return print(self.userString.upper())
+
+instanceOfStringsClass = Strings("hi")
+print(instanceOfStringsClass.get_String()) # expected result 'hi'
+instanceOfStringsClass.print_String() # expected result 'HI'
