79c163ef36c815775f2c15bdcb5fca6296c1788c
jbframe <framejb@gmail.com>
2021-05-28 23:18:09 -0600

    Added Week 4 questions.

---
+++ Week4/frame_J_HW4_june_5/question1.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# Assignment 4
+# Question 1
+# Write a Python multiprocessing program that runs all five functions below. Use a time.counter to tests the amount of time it takes all functions to run synchronously and then concurrently
+
+
+# def add(x, y):
+
+#     time.sleep(1)
+
+#     return x + y
+
+# def subtract(x, y):
+
+#    time.sleep(1)
+#     return x-y
+
+# def multiply(x, y):
+
+#    time.sleep(1)
+#     return x * y
+
+# def divide(x, y):
+
+#     time.sleep(1)
+#     if y==0:
+#         raise ValueError('Cannot divide by zero!')
+#     return x / y
+
+# def modulus(x, y):
+
+#     time.sleep(1)
+#     return x % y
+
+
+
+# Note: As a more advanced example of multiprocessing you may want to check the following file: ImageProcessingExample
