ee30a8a7157a53f485b118c25d2d85193734bca0
jbframe <framejb@gmail.com>
2021-05-29 23:26:30 -0600

    Question 1 synchronously executed.

--- Week4/frame_J_HW4_june_5/question1.py
+++ Week4/frame_J_HW4_june_5/question1.py
@@ -2,36 +2,38 @@
 # Assignment 4
 # Question 1
 # Write a Python multiprocessing program that runs all five functions below. Use a time.counter to tests the amount of time it takes all functions to run synchronously and then concurrently
-
-
-# def add(x, y):
-
-#     time.sleep(1)
-
-#     return x + y
-
-# def subtract(x, y):
-
-#    time.sleep(1)
-#     return x-y
-
-# def multiply(x, y):
-
-#    time.sleep(1)
-#     return x * y
-
-# def divide(x, y):
-
-#     time.sleep(1)
-#     if y==0:
-#         raise ValueError('Cannot divide by zero!')
-#     return x / y
-
-# def modulus(x, y):
-
-#     time.sleep(1)
-#     return x % y
-
-
-
 # Note: As a more advanced example of multiprocessing you may want to check the following file: ImageProcessingExample
+import time
+start = time.perf_counter()
+def add(x, y):
+  time.sleep(1)
+  return x + y
+
+def subtract(x, y):
+  time.sleep(1)
+  return x-y
+
+def multiply(x, y):
+  time.sleep(1)
+  return x * y
+
+def divide(x, y):
+  time.sleep(1)
+  if y==0:
+      raise ValueError('Cannot divide by zero!')
+  return x / y
+
+def modulus(x, y):
+  time.sleep(1)
+  return x % y
+
+x = 1000000000
+y = 123456789123456789
+add(x,y)
+subtract(x,y)
+multiply(x,y)
+divide(x,y)
+modulus(x,y)
+
+finish = time.perf_counter()
+print("Finished in ", round(finish-start, 3), ' seconds')
