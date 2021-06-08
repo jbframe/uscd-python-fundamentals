12eb45fdc10d68fd9044494bd5ea23ca12d7daec
jbframe <framejb@gmail.com>
2021-06-07 18:03:54 -0600

    Added week 5 discussion.

---
+++ Week5/Examples/discussion.py
@@ -0,0 +1,1 @@
+# Python has many different options for working with data such as Pandas. To extract data from the internet, Python offers a library called  BeautifulSoup. Research how to use BeautifulSoup for Webscraping. Give some examples.
